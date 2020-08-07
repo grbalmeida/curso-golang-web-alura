74bb8957694bb5e3ac841c501810381a3dd10ef5
gilvan <g.r.almeida@live.com>
2020-08-07 10:56:58 -0300

    Criando struct de produtos

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 	"net/http"
 )
 
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
 var templates = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
@@ -13,5 +20,12 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "Index", nil)
+	produtos := []Produto{
+		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 5},
+		{Nome: "Tênis", Descricao: "Confortável", Preco: 89, Quantidade: 3},
+		{Nome: "Fone", Descricao: "Muito bom", Preco: 59, Quantidade: 2},
+		{Nome: "Produto novo", Descricao: "Muito legal", Preco: 1.99, Quantidade: 1},
+	}
+
+	templates.ExecuteTemplate(w, "Index", produtos)
 }
