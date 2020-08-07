7853e1fd1325b1b21438fff94a5022cb91041fa9
gilvan <g.r.almeida@live.com>
2020-08-07 17:22:09 -0300

    Modularizando o código

--- main.go
+++ main.go
@@ -1,74 +1,20 @@
 package main
 
 import (
-	"database/sql"
 	"html/template"
 	"net/http"
 
-	_ "github.com/lib/pq"
+	"github.com/grbalmeida/curso-golang-web-alura/models"
 )
-
-func conectaComBancoDeDados() *sql.DB {
-	conexao := "user=postgres dbname=alura_loja password=root host=localhost sslmode=disable"
-
-	db, err := sql.Open("postgres", conexao)
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	return db
-}
-
-type Produto struct {
-	Id         int
-	Nome       string
-	Descricao  string
-	Preco      float64
-	Quantidade int
-}
 
 var templates = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
-	db := conectaComBancoDeDados()
-	defer db.Close()
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	db := conectaComBancoDeDados()
-
-	selectDeTodosOsProdutos, err := db.Query("select * from produtos order by id")
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	p := Produto{}
-	produtos := []Produto{}
-
-	for selectDeTodosOsProdutos.Next() {
-		var id, quantidade int
-		var nome, descricao string
-		var preco float64
-
-		err = selectDeTodosOsProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
-
-		if err != nil {
-			panic(err.Error())
-		}
-
-		p.Id = id
-		p.Nome = nome
-		p.Descricao = descricao
-		p.Preco = preco
-		p.Quantidade = quantidade
-
-		produtos = append(produtos, p)
-	}
-
-	templates.ExecuteTemplate(w, "Index", produtos)
-	defer db.Close()
+	todosOsProdutos := models.BuscaTodosOsProdutos()
+	templates.ExecuteTemplate(w, "Index", todosOsProdutos)
 }
